66b78b560adb2b7f73beab12434cf25e177259a8
Su8Zer0 <su8zer0@poczta.fm>
2022-06-07 09:59:28 +0200

    - basic configuration - gekiyasu scraper npe fix

--- Providers/Scrapers/IScraper.cs
+++ Providers/Scrapers/IScraper.cs
@@ -3,7 +3,6 @@ using MediaBrowser.Controller.Entities;
 using MediaBrowser.Controller.Entities.Movies;
 using MediaBrowser.Controller.Providers;
 using MediaBrowser.Model.Entities;
-using MediaBrowser.Model.MediaInfo;
 using MediaBrowser.Model.Providers;
 
 namespace Jellyfin.Plugin.IvInfo.Providers.Scrapers
@@ -11,22 +10,23 @@ namespace Jellyfin.Plugin.IvInfo.Providers.Scrapers
     public interface IScraper
     {
         /// <summary>
-        /// Gets scraper priority, used in filling metadata. Scrapers with lower priority are called earlier.
+        ///     Gets scraper priority, used in filling metadata. Scrapers with lower priority are called earlier.
         /// </summary>
         /// <returns>scraper priority</returns>
         public int Priority { get; }
-        
+
         public bool Enabled { get; }
 
         /// <summary>
-        /// Returns search results for this item info.
+        ///     Returns search results for this item info.
         /// </summary>
         /// <param name="info">searched item</param>
         /// <returns>list with search results</returns>
         public IEnumerable<RemoteSearchResult> GetSearchResults(MovieInfo info);
 
         /// <summary>
-        /// Fills metadata for <see cref="MetadataResult{T}"/>. When <c>overwrite</c> is true then all (available) data is filled, otherwise only missing data is filled.
+        ///     Fills metadata for <see cref="MetadataResult{T}" />. When <c>overwrite</c> is true then all (available) data is
+        ///     filled, otherwise only missing data is filled.
         /// </summary>
         /// <param name="metadata">metadata object to fill</param>
         /// <param name="overwrite">to overwrite present data or not</param>
@@ -34,26 +34,29 @@ namespace Jellyfin.Plugin.IvInfo.Providers.Scrapers
         public bool FillMetadata(MetadataResult<Movie> metadata, bool overwrite = false);
 
         /// <summary>
-        /// Gets url list for image for passed item and requested type.
+        ///     Gets url list for image for passed item and requested type.
         /// </summary>
         /// <param name="item">item</param>
-        /// <param name="imageType">image type <seealso cref="ImageType"/></param>
+        /// <param name="imageType">image type <seealso cref="ImageType" /></param>
         /// <param name="overwrite"></param>
         /// <returns>url list for image or empty list if image was not found</returns>
         public IEnumerable<RemoteImageInfo> GetImages(BaseItem item, ImageType imageType = ImageType.Primary,
             bool overwrite = false);
 
         /// <summary>
-        /// Returns array of <see cref="ImageType"/>s provided by scraper.
+        ///     Returns array of <see cref="ImageType" />s provided by scraper.
         /// </summary>
-        /// <returns>array of <see cref="ImageType"/>s</returns>
+        /// <returns>array of <see cref="ImageType" />s</returns>
         public IEnumerable<ImageType> HandledImageTypes();
 
         /// <summary>
-        /// Adds new image to list, if image of this type already exists in the list and overwrite is true then existing image is removed and new one added, otherwise nothing is done.
+        ///     Adds new image to list, if image of this type already exists in the list and overwrite is true then existing image
+        ///     is removed and new one added, otherwise nothing is done.
         /// </summary>
         /// <param name="images">list with images</param>
-        /// <param name="imageType"><see cref="ImageType"/></param>
+        /// <param name="imageType">
+        ///     <see cref="ImageType" />
+        /// </param>
         /// <param name="url">new image url</param>
         /// <param name="overwrite">should the image be overwritten if it exists</param>
         /// <returns>list of images, modified if necessary</returns>
