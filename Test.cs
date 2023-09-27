22bef5bfaba422cde4e0519043e7423de436282d
SubZeroPL <subzeropl@gmail.com>
2023-09-27 12:33:45 +0200

    Small fix for JavLibrary entries without pictures

--- Test.cs
+++ Test.cs
@@ -5,40 +5,45 @@ using MediaBrowser.Controller.Providers;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 
-namespace Jellyfin.Plugin.IvInfo
+namespace Jellyfin.Plugin.IvInfo;
+
+/*
+ * Scrapers to add?
+ *  - http://warashi-asian-pornstars.fr/en/s-0/wapdb-database-of-asian-pornstars-japanese-av-actresses-and-actors
+ *  - https://javtext.net/
+ */
+
+internal static class Test
 {
-    internal static class Test
+    [STAThread]
+    private static void Main()
     {
-        [STAThread]
-        private static void Main()
+        Console.Out.WriteLine("Test");
+        var l = new NullLoggerFactory().CreateLogger<IvInfoProvider>();
+        var p = new IvInfoProvider(null!, l);
+        var mi = new MovieInfo
         {
-            Console.Out.WriteLine("Test");
-            var l = new NullLoggerFactory().CreateLogger<IvInfoProvider>();
-            var p = new IvInfoProvider(null!, l);
-            var mi = new MovieInfo
-            {
-                Name = "Name",
-                Path = "/media/iv/[REBD-700].mkv",
-                MetadataCountryCode = "EN",
-                MetadataLanguage = "en"
-            };
-            // mi.SetProviderId("JavlibraryScraper", "javme3a55i");
-            // mi.SetProviderId(IvInfoConstants.Name, "OAE-205");
-            // var sr = p.GetSearchResults(mi, CancellationToken.None).ConfigureAwait(false).GetAwaiter().GetResult();
-            // foreach (var r in sr)
-            // {
-            //     foreach (var id in r.ProviderIds)
-            //     {
-            //         if (!mi.ProviderIds.ContainsKey(id.Key))
-            //         {
-            //             mi.ProviderIds.Add(id.Key, id.Value);
-            //         }
-            //     }
-            // }
-            var mr = p.GetMetadata(mi, CancellationToken.None).ConfigureAwait(false).GetAwaiter().GetResult();
-            mr.Item.PreferredMetadataLanguage = "en";
-            mr.Item.PreferredMetadataCountryCode = "EN";
-            // p.GetImages(mr.Item, CancellationToken.None).ConfigureAwait(false).GetAwaiter().GetResult();
-        }
+            Name = "Name",
+            Path = "/media/iv/[REBD-700].mkv",
+            MetadataCountryCode = "EN",
+            MetadataLanguage = "en"
+        };
+        // mi.SetProviderId("JavlibraryScraper", "javme3a55i");
+        // mi.SetProviderId(IvInfoConstants.Name, "OAE-205");
+        // var sr = p.GetSearchResults(mi, CancellationToken.None).ConfigureAwait(false).GetAwaiter().GetResult();
+        // foreach (var r in sr)
+        // {
+        //     foreach (var id in r.ProviderIds)
+        //     {
+        //         if (!mi.ProviderIds.ContainsKey(id.Key))
+        //         {
+        //             mi.ProviderIds.Add(id.Key, id.Value);
+        //         }
+        //     }
+        // }
+        var mr = p.GetMetadata(mi, CancellationToken.None).ConfigureAwait(false).GetAwaiter().GetResult();
+        mr.Item.PreferredMetadataLanguage = "en";
+        mr.Item.PreferredMetadataCountryCode = "EN";
+        // p.GetImages(mr.Item, CancellationToken.None).ConfigureAwait(false).GetAwaiter().GetResult();
     }
 }
