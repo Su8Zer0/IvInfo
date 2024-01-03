47f907ad594dbaa33f5e1f33502db670a9543206
SubZeroPL <subzeropl@gmail.com>
2024-01-03 11:44:04 +0100

    Added unit tests
    - removed Test class as its covered by unit tests now

---
+++ Tests/TestConsts.cs
@@ -0,0 +1,15 @@
+﻿namespace Jellyfin.Plugin.IvInfo.Tests;
+
+public class TestConsts
+{
+    public const string Name = "Name";
+    public const string Lang = "en";
+    public const string GoodPath = "/media/iv/[REBD-789].mkv";
+    public const string GoodGlobalId = "REBD-789";
+    public const string BadPath = "/media/iv/[ASDF-000].mkv";
+    public const string BadGlobalId = "ASDF-000";
+    public const string MultipleResultsPath = "/media/iv/[SS-049].mkv";
+    public const string JavlibraryScraperId = "javmema374";
+    public const string DmmScraperId = "https://www.dmm.co.jp/digital/videoa/-/detail/=/cid=h_346rebd00789/";
+    public const string GekiyasuScraperId = "products/detail39857";
+}
