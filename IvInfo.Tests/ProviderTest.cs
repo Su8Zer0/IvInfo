581a61d194b5ab490fa76071e0728aed2ec7fa72
Su8Zer0 <su8zer0@poczta.fm>
2024-02-06 13:39:23 +0100

    Unit tests
    - moved scraper tests to single class
    - added basic provider tests

---
+++ IvInfo.Tests/ProviderTest.cs
@@ -0,0 +1,42 @@
+﻿using IvInfo.Providers;
+using MediaBrowser.Controller.Entities;
+using MediaBrowser.Controller.Entities.Audio;
+using MediaBrowser.Controller.Entities.Movies;
+using MediaBrowser.Controller.Entities.TV;
+using Microsoft.Extensions.Logging;
+
+namespace IvInfo.Tests;
+
+[TestFixture]
+public class ProviderTest
+{
+    [Test]
+    public void ShouldSupportMovies()
+    {
+        var logger = new LoggerFactory().CreateLogger<IvInfoProvider>();
+        var provider = new IvInfoProvider(null!, logger);
+        var item = new Movie();
+        Assert.That(provider.Supports(item), Is.True);
+    }
+    
+    [Test]
+    public void ShouldNotSupportAudio()
+    {
+        var logger = new LoggerFactory().CreateLogger<IvInfoProvider>();
+        var provider = new IvInfoProvider(null!, logger);
+        var item = new Audio();
+        Assert.That(provider.Supports(item), Is.False);
+    }
+    
+    [Test]
+    public void ShouldNotSupportTv()
+    {
+        var logger = new LoggerFactory().CreateLogger<IvInfoProvider>();
+        var provider = new IvInfoProvider(null!, logger);
+        var items = new BaseItem[] { new Episode(), new Season(), new Series() };
+        foreach (var item in items)
+        {
+            Assert.That(provider.Supports(item), Is.False);   
+        }
+    }
+}
