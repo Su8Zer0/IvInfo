e56d95ede5a965abc87abacb9f0aa67ae9b1b72c
Su8Zer0 <su8zer0@poczta.fm>
2024-02-14 11:19:26 +0100

    build: changed path in debug script

--- IvInfo/scripts/CopyDebug.ps1
+++ IvInfo/scripts/CopyDebug.ps1
@@ -4,13 +4,13 @@ $CUR_DIR = Get-Location
 #Set-Location ..
 dotnet publish --configuration Debug
 Start-Process "pskill" -Wait -NoNewWindow -ArgumentList "-nobanner jellyfin.exe"
-Start-Sleep -Seconds 2
+Start-Sleep -Seconds 1
 New-Item -Path "$( $JF_DIR )\data\plugins\IvInfo\" -ItemType Directory -ErrorAction SilentlyContinue
-Copy-Item ".\bin\Debug\net6.0\IvInfo.dll" -Destination "$( $JF_DIR )\data\plugins\IvInfo\"
-Copy-Item ".\bin\Debug\net6.0\IvInfo.pdb" -Destination "$( $JF_DIR )\data\plugins\IvInfo\"
-Copy-Item ".\bin\Debug\net6.0\publish\F23.StringSimilarity.dll" -Destination "$( $JF_DIR )\data\plugins\IvInfo\"
-Copy-Item ".\bin\Debug\net6.0\publish\AngleSharp.dll" -Destination "$( $JF_DIR )\data\plugins\IvInfo\"
-Copy-Item ".\bin\Debug\net6.0\publish\AngleSharp.XPath.dll" -Destination "$( $JF_DIR )\data\plugins\IvInfo\"
+Copy-Item ".\IvInfo\bin\Debug\net6.0\IvInfo.dll" -Destination "$( $JF_DIR )\data\plugins\IvInfo\"
+Copy-Item ".\IvInfo\bin\Debug\net6.0\IvInfo.pdb" -Destination "$( $JF_DIR )\data\plugins\IvInfo\"
+Copy-Item ".\IvInfo\bin\Debug\net6.0\publish\F23.StringSimilarity.dll" -Destination "$( $JF_DIR )\data\plugins\IvInfo\"
+Copy-Item ".\IvInfo\bin\Debug\net6.0\publish\AngleSharp.dll" -Destination "$( $JF_DIR )\data\plugins\IvInfo\"
+Copy-Item ".\IvInfo\bin\Debug\net6.0\publish\AngleSharp.XPath.dll" -Destination "$( $JF_DIR )\data\plugins\IvInfo\"
 #Copy-Item ".\meta.json" -Destination "$( $JF_DIR )\data\plugins\IvInfo\"
 Set-Location $CUR_DIR
 Start-Process "$( $JF_DIR )\jellyfin.exe" -Wait -WorkingDirectory $JF_DIR -NoNewWindow -ArgumentList "--datadir $( $JF_DIR )\data"
