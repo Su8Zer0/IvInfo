c4b10fa8b61ea57966858d1e6d66d99ca3aa6153
Su8Zer0 <su8zer0@poczta.fm>
2024-02-15 10:31:31 +0100

    better handling of plugin version

--- IvInfoConstants.cs
+++ IvInfoConstants.cs
@@ -3,5 +3,6 @@
 public static class IvInfoConstants
 {
     public const string Name = "IvInfo";
+    public const string Description = "Idol Video metadata provider";
     public const string Guid = "abe0c619-a145-4890-896b-cb1089ade4fe";
 }
