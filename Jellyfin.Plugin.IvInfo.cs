d6772c638f074f74c66bf65e767f3ac24d63a467
Su8Zer0 <su8zer0@poczta.fm>
2024-02-15 10:31:32 +0100

    Some code cleanups
    - removed unused script
    - changed regex for ID parsing
    - extracting of plugin version from assembly removed as its done by base plugin code anyway

--- Jellyfin.Plugin.IvInfo.cs
+++ Jellyfin.Plugin.IvInfo.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Reflection;
-using System.Text.Json;
 using Jellyfin.Plugin.IvInfo.Configuration;
 using MediaBrowser.Common.Configuration;
 using MediaBrowser.Common.Plugins;
@@ -46,18 +43,6 @@ public class IvInfo : BasePlugin<IvInfoPluginConfiguration>, IHasWebPages
 
     public override PluginInfo GetPluginInfo()
     {
-        try
-        {
-            var thisAssem = typeof(IvInfo).Assembly;
-            var thisAssemName = thisAssem.GetName();
-            var ver = thisAssemName.Version ?? new Version("");
-            return new PluginInfo(Name, ver, IvInfoConstants.Description, Id, true);
-        }
-        catch (Exception e)
-        {
-            Console.Error.WriteLine($"Couldn't read version info from assembly\n{e.Message}");
-        }
-        
         return new PluginInfo(Name, Version, IvInfoConstants.Description, Id, true);
     }
 }
