c2df699a47c609b1ae7b022488adcc5906b1654f
Su8Zer0 <su8zer0@poczta.fm>
2024-02-15 10:31:30 +0100

    Changes: - working Gekiyasu scraper - version 1.0 - metadata for plugin - manifest for repository - .gitignore

--- Providers/Scrapers/GekiyasuScraper.cs
+++ Providers/Scrapers/GekiyasuScraper.cs
@@ -15,12 +15,13 @@ namespace Jellyfin.Plugin.IvInfo.Providers.Scrapers
     public class GekiyasuScraper : IScraper
     {
         private const string Name = nameof(GekiyasuScraper);
-        
+
         private const string BaseUrl = "https://www.gekiyasu-dvdshop.jp/";
         private const string PageUrl = BaseUrl + "products/{0}";
         private const string SearchUrl = BaseUrl + "products/list.php?mode=search&name={0}";
         private const string NoResults = "該当件数0件です";
         private const string MakerLbl = "メーカー";
+        private const string Selector = "//div[@class='b-list-area']/*/*/*/a[@class='thumbnail']";
 
         private readonly ILogger _logger;
 
@@ -31,13 +32,51 @@ namespace Jellyfin.Plugin.IvInfo.Providers.Scrapers
 
         public int Priority => 4;
 
-        public bool Enabled => false;
+        public bool Enabled => true;
 
         public IEnumerable<RemoteSearchResult> GetSearchResults(MovieInfo info)
         {
-            var globalId = info.GetProviderId(IvInfoConstants.Name);
+            var globalId = info.GetProviderId(IvInfoConstants.Name) ?? IvInfoProvider.GetId(info);
             _logger.LogDebug("{Name}: searching for id: {Id}", Name, globalId);
-            yield break;
+            if (string.IsNullOrEmpty(globalId)) yield break;
+
+            var (multiple, doc) = GetSearchResultsPage(globalId);
+            if (string.IsNullOrEmpty(doc.Text)) yield break;
+            if (multiple)
+            {
+                var nodeCollection =
+                    doc.DocumentNode.SelectNodes("//div[@class='b-list-area']/ul[@class='clearfix']/li");
+                foreach (var node in nodeCollection)
+                {
+                    var scraperId = node.ChildNodes.FindFirst("a").GetAttributeValue("href", "").Split("/").Last();
+                    var title = node.ChildNodes.FindFirst("p").InnerText;
+                    var imgUrl = BaseUrl + node.ChildNodes.FindFirst("a").FirstChild.GetAttributeValue("src", "");
+                    var result = new RemoteSearchResult
+                    {
+                        Name = title,
+                        ImageUrl = imgUrl,
+                        SearchProviderName = Name
+                    };
+                    result.SetProviderId(Name, scraperId);
+                    yield return result;
+                }
+            }
+            else
+            {
+                doc = GetFirstResult(doc);
+                var scraperId = GetScraperId(doc);
+                var title = doc.DocumentNode.SelectSingleNode("//div[@class='b-title']/strong")?.InnerText;
+                var imgUrl = BaseUrl + doc.DocumentNode.SelectSingleNode("//a[@class='thumbnail']")
+                    .GetAttributeValue("href", "");
+                var result = new RemoteSearchResult
+                {
+                    Name = title,
+                    ImageUrl = imgUrl,
+                    SearchProviderName = Name
+                };
+                result.SetProviderId(Name, scraperId);
+                yield return result;
+            }
         }
 
         public bool FillMetadata(MetadataResult<Movie> metadata, bool overwrite = false)
@@ -45,15 +84,20 @@ namespace Jellyfin.Plugin.IvInfo.Providers.Scrapers
             var scraperId = metadata.Item.GetProviderId(Name);
             var globalId = metadata.Item.GetProviderId(IvInfoConstants.Name);
 
-            var web = new HtmlWeb();
-            var doc = scraperId != null ? GetSingleResult(scraperId) : GetSearchResultsPage(globalId!); 
-            
+            HtmlDocument doc;
+            if (scraperId == null)
+            {
+                (_, doc) = GetSearchResultsPage(globalId!);
+                doc = GetFirstResult(doc);
+            }
+            else
+            {
+                doc = GetSingleResult(scraperId);
+            }
+
             if (string.IsNullOrEmpty(doc.Text)) return false;
 
-            var resultUrl = BaseUrl + doc.DocumentNode.SelectSingleNode("//a[@class='thumbnail']")
-                .GetAttributeValue("href", "");
-            doc = web.Load(resultUrl);
-            if (doc == null) return false;
+            scraperId = GetScraperId(doc);
 
             var title = doc.DocumentNode.SelectSingleNode("//div[@class='b-title']/strong")?.InnerText;
             var datePresent = DateTime.TryParse(
@@ -77,6 +121,8 @@ namespace Jellyfin.Plugin.IvInfo.Providers.Scrapers
             if (overwrite || string.IsNullOrWhiteSpace(metadata.Item.ExternalId)) metadata.Item.ExternalId = scraperId;
             if (overwrite || metadata.Item.Studios.Length == 0) metadata.Item.AddStudio(maker);
 
+            metadata.Item.SetProviderId(Name, scraperId);
+
             return true;
         }
 
@@ -87,18 +133,15 @@ namespace Jellyfin.Plugin.IvInfo.Providers.Scrapers
 
             if (!HandledImageTypes().Contains(imageType)) return result;
 
+            if (item.ImageInfos.Any(i => i.Type == imageType) && !overwrite) return result;
+
             var scraperId = item.GetProviderId(Name);
             if (string.IsNullOrEmpty(scraperId)) return result;
 
             var web = new HtmlWeb();
-            var doc = web.Load(string.Format(SearchUrl, scraperId));
+            var doc = web.Load(string.Format(PageUrl, scraperId));
 
             if (doc.DocumentNode.InnerText.Contains(NoResults)) return result;
-
-            var resultUrl = BaseUrl + doc.DocumentNode.SelectSingleNode("//a[@class='thumbnail']")
-                .GetAttributeValue("href", "");
-            doc = web.Load(resultUrl);
-            if (doc == null) return result;
 
             var thumbUrl = BaseUrl + doc.DocumentNode.SelectSingleNode("//a[@class='thumbnail']")
                 .GetAttributeValue("href", "");
@@ -121,24 +164,35 @@ namespace Jellyfin.Plugin.IvInfo.Providers.Scrapers
             yield return ImageType.Box;
         }
 
-        /// <summary>
-        /// Returns page with search results. If nothing was found returns empty page.
-        /// </summary>
-        /// <param name="globalId">global id</param>
-        /// <returns>HtmlDocument</returns>
-        private HtmlDocument GetSearchResultsPage(string globalId)
+        private static bool HasMultipleResults(HtmlDocument doc)
         {
-            _logger.LogDebug("{Name}: searching for id: {Id}", Name, globalId);
-            var doc = new HtmlDocument();
-            if (string.IsNullOrEmpty(globalId)) return doc;
-            var url = string.Format(SearchUrl, globalId);
-            var web = new HtmlWeb();
-            doc = web.Load(url);
-            return doc.DocumentNode.InnerText.Contains(NoResults) ? new HtmlDocument() : doc;
+            var results = doc.DocumentNode.SelectNodes(Selector);
+            return results.Count > 1;
         }
 
         /// <summary>
-        /// Returns result page for specific scraper id.
+        /// Returns page with search results and true if there are multiple results or false if only one.
+        /// If nothing was found returns empty page and false.
+        /// </summary>
+        /// <param name="globalId">global id</param>
+        /// <returns>bool, HtmlDocument pair</returns>
+        private (bool, HtmlDocument) GetSearchResultsPage(string globalId)
+        {
+            _logger.LogDebug("{Name}: searching for id: {Id}", Name, globalId);
+            var doc = new HtmlDocument();
+            if (string.IsNullOrEmpty(globalId)) return (false, doc);
+            var url = string.Format(SearchUrl, globalId);
+            var web = new HtmlWeb();
+            doc = web.Load(url);
+            if (string.IsNullOrEmpty(doc.Text)) return (false, doc);
+
+            return doc.DocumentNode.InnerText.Contains(NoResults)
+                ? (false, new HtmlDocument())
+                : (HasMultipleResults(doc), doc);
+        }
+
+        /// <summary>
+        /// Returns result page for specific scraper id or empty page if not found.
         /// </summary>
         /// <param name="scraperId">scraper id</param>
         /// <returns>page for id</returns>
@@ -151,6 +205,24 @@ namespace Jellyfin.Plugin.IvInfo.Providers.Scrapers
             var web = new HtmlWeb();
             doc = web.Load(url);
             return doc;
+        }
+
+        private static HtmlDocument GetFirstResult(HtmlDocument doc)
+        {
+            var result = new HtmlDocument();
+            if (string.IsNullOrEmpty(doc.Text)) return result;
+            if (HasMultipleResults(doc)) return result;
+
+            var resultUrl = BaseUrl + doc.DocumentNode.SelectSingleNode(Selector).GetAttributeValue("href", "");
+            var web = new HtmlWeb();
+            result = web.Load(resultUrl);
+            return result;
+        }
+
+        private static string GetScraperId(HtmlDocument doc)
+        {
+            return doc.DocumentNode.SelectSingleNode("//form[@id='form1']")?.GetAttributeValue("action", "").Split("/")
+                .Last() ?? string.Empty;
         }
     }
 }
--- Test.cs
+++ Test.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Threading;
 using Jellyfin.Plugin.IvInfo.Providers;
-using Jellyfin.Plugin.IvInfo.Providers.Scrapers;
 using MediaBrowser.Controller.Providers;
 using MediaBrowser.Model.Entities;
 using Microsoft.Extensions.Logging;
@@ -20,10 +19,10 @@ namespace Jellyfin.Plugin.IvInfo
             var mi = new MovieInfo
             {
                 Name = "Name",
-                Path = "/media/iv/[REBDB-621].mp4"
+                Path = "/media/iv/[IMBD-168].mp4"
             };
             // mi.SetProviderId("JavlibraryScraper", "javme3a55i");
-            // mi.SetProviderId(IvInfoConstants.Name, "SS-043");
+            mi.SetProviderId(IvInfoConstants.Name, "IMBD-168");
             p.GetSearchResults(mi, CancellationToken.None);
             // var mr = p.GetMetadata(mi, CancellationToken.None).ConfigureAwait(false).GetAwaiter().GetResult();
             // mr.Item.PreferredMetadataLanguage = "en";
