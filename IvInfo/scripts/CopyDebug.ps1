0dfcfd8e895876256d7982154ea9605e9988e3e4
Su8Zer0 <su8zer0@poczta.fm>
2025-12-15 22:36:47 +0100

    build: updated versions
    updated jf version
    updated dependencies
    switched to net 9.0

--- IvInfo/scripts/CopyDebug.ps1
+++ IvInfo/scripts/CopyDebug.ps1
@@ -6,11 +6,12 @@ dotnet publish --configuration Debug
 Start-Process "pskill" -Wait -NoNewWindow -ArgumentList "-nobanner jellyfin.exe"
 Start-Sleep -Seconds 1
 New-Item -Path "$( $JF_DIR )\data\plugins\IvInfo\" -ItemType Directory -ErrorAction SilentlyContinue
-Copy-Item ".\IvInfo\bin\Debug\net8.0\IvInfo.dll" -Destination "$( $JF_DIR )\data\plugins\IvInfo\"
-Copy-Item ".\IvInfo\bin\Debug\net8.0\IvInfo.pdb" -Destination "$( $JF_DIR )\data\plugins\IvInfo\"
-Copy-Item ".\IvInfo\bin\Debug\net8.0\publish\F23.StringSimilarity.dll" -Destination "$( $JF_DIR )\data\plugins\IvInfo\"
-Copy-Item ".\IvInfo\bin\Debug\net8.0\publish\AngleSharp.dll" -Destination "$( $JF_DIR )\data\plugins\IvInfo\"
-Copy-Item ".\IvInfo\bin\Debug\net8.0\publish\AngleSharp.XPath.dll" -Destination "$( $JF_DIR )\data\plugins\IvInfo\"
+Copy-Item ".\IvInfo\bin\Debug\net9.0\IvInfo.dll" -Destination "$( $JF_DIR )\data\plugins\IvInfo\"
+Copy-Item ".\IvInfo\bin\Debug\net9.0\IvInfo.pdb" -Destination "$( $JF_DIR )\data\plugins\IvInfo\"
+Copy-Item ".\IvInfo\bin\Debug\net9.0\publish\F23.StringSimilarity.dll" -Destination "$( $JF_DIR )\data\plugins\IvInfo\"
+Copy-Item ".\IvInfo\bin\Debug\net9.0\publish\AngleSharp.dll" -Destination "$( $JF_DIR )\data\plugins\IvInfo\"
+Copy-Item ".\IvInfo\bin\Debug\net9.0\publish\AngleSharp.XPath.dll" -Destination "$( $JF_DIR )\data\plugins\IvInfo\"
+Copy-Item ".\IvInfo\bin\Debug\net9.0\publish\FlareSolverrSharp.dll" -Destination "$( $JF_DIR )\data\plugins\IvInfo\"
 #Copy-Item ".\meta.json" -Destination "$( $JF_DIR )\data\plugins\IvInfo\"
 Set-Location $CUR_DIR
 Start-Process "$( $JF_DIR )\jellyfin.exe" -Wait -WorkingDirectory $JF_DIR -NoNewWindow -ArgumentList "--datadir $( $JF_DIR )\data"
